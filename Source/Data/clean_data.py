7584da4a8e7039e2b780c1ee6d282cae54ac62d8
ParthivDataScientist <parthivpatel684@gmail.com>
2025-09-05 12:25:43 +0530

    WUFK-17 Fix null pointer issue in cleaning

--- Source/Data/clean_data.py
+++ Source/Data/clean_data.py
@@ -10,7 +10,7 @@ from Source.Utils.helpers import load_data
 from Logging.logger import get_logger
 import json
 from datetime import datetime 
-# This is jira change for bug
+# This is jira change for bug for again this is jira
 
 # ----------------------------Config File------------------------------#
 with open(r'Z:\\Supply-Chain_management(SCM)\\Source\\config.json',"r") as f:
